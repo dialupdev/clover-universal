ec08a23024cda07464bb10906f2b9de24b03034e
Steve Davis <steve@dialup.dev>
2017-06-15 17:42:56 -0400

    Load notes in Master view and link to detail view by note ID

--- Lido/View Controllers/MasterViewController.swift
+++ Lido/View Controllers/MasterViewController.swift
@@ -10,19 +10,21 @@ import UIKit
 
 class MasterViewController: UITableViewController {
   var detailViewController: DetailViewController? = nil
-  var objects = [Any]()
+  var notes = [Note]()
 
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     navigationItem.leftBarButtonItem = editButtonItem
 
-    let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(insertNewObject(_:)))
-    navigationItem.rightBarButtonItem = addButton
-
     if let split = splitViewController {
       let controllers = split.viewControllers
       detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+    }
+
+    Note.all() { result in
+      self.notes = result
+      self.tableView.reloadData()
     }
   }
 
@@ -36,19 +38,14 @@ class MasterViewController: UITableViewController {
     // Dispose of any resources that can be recreated.
   }
 
-  func insertNewObject(_ sender: Any) {
-    objects.insert(NSDate(), at: 0)
-    let indexPath = IndexPath(row: 0, section: 0)
-    tableView.insertRows(at: [indexPath], with: .automatic)
-  }
-
   // MARK: - Segues
 
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == "showDetail" {
       if let indexPath = tableView.indexPathForSelectedRow {
+        let note = notes[indexPath.row]
         let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
-        controller.noteId = indexPath.row
+        controller.noteId = note.id
         controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
         controller.navigationItem.leftItemsSupplementBackButton = true
       }
@@ -62,14 +59,14 @@ class MasterViewController: UITableViewController {
   }
 
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return objects.count
+    return notes.count
   }
 
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-    let object = objects[indexPath.row] as! NSDate
-    cell.textLabel!.text = object.description
+    let object = notes[indexPath.row]
+    cell.textLabel!.text = object.title
     return cell
   }
 
@@ -77,17 +74,4 @@ class MasterViewController: UITableViewController {
     // Return false if you do not want the specified item to be editable.
     return true
   }
-
-  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-    if editingStyle == .delete {
-      objects.remove(at: indexPath.row)
-      tableView.deleteRows(at: [indexPath], with: .fade)
-    }
-    else if editingStyle == .insert {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-  }
-
-
 }
-
